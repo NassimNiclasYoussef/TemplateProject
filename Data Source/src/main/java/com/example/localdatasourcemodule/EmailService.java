822ef7ac243cdd1e76f9a0ff00177a23418d0cf3
NassimNiclasYoussef <nassimniclasyoussef@gmail.com>
2019-02-16 18:07:27 +0200

    integration firebase phase 3

--- Data Source/src/main/java/com/example/localdatasourcemodule/EmailService.java
+++ Data Source/src/main/java/com/example/localdatasourcemodule/EmailService.java
@@ -1,7 +1,34 @@
 package com.example.localdatasourcemodule;
 
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.functions.FirebaseFunctions;
+import com.google.firebase.functions.HttpsCallableResult;
+
+import java.util.HashMap;
+
 public class EmailService {
 
     private EmailService() {
     }
+
+    public void sendEmail(String msg) {
+        HashMap<String, String> data = new HashMap<>();
+        data.put("msg", msg);
+        FirebaseFunctions.getInstance()
+                .getHttpsCallable("sendWelcomeEmail")
+                .call(data)
+                .continueWith(new Continuation<HttpsCallableResult, String>() {
+                    @Override
+                    public String then(@NonNull Task<HttpsCallableResult> task) throws Exception {
+                        Log.e("sendWelcomeEmail", task.getResult().getData().toString());
+                        return (String) task.getResult().getData();
+                    }
+                });
+    }
+
 }
