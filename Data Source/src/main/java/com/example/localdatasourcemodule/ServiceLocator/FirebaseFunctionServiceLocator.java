e75a904165a50d7ecb539efcaeeeb79a57019052
NassimNiclasYoussef <nassimniclasyoussef@gmail.com>
2019-02-16 17:53:56 +0200

    integration firebase phase 2

--- Data Source/src/main/java/com/example/localdatasourcemodule/ServiceLocator/FirebaseFunctionServiceLocator.java
+++ Data Source/src/main/java/com/example/localdatasourcemodule/ServiceLocator/FirebaseFunctionServiceLocator.java
@@ -1,6 +1,13 @@
 package com.example.localdatasourcemodule.ServiceLocator;
 
+import com.example.localdatasourcemodule.EmailService;
+import com.example.localdatasourcemodule.Service;
+
 public class FirebaseFunctionServiceLocator {
+    @Service
+    private EmailService emailService;
 
-
+    public EmailService getEmailService() {
+        return emailService;
+    }
 }
