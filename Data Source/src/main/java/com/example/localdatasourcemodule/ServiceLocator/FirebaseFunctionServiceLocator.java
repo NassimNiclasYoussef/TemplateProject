5c135744b774014cf12a468ce6e79ca781d0292a
NassimNiclasYoussef <nassimniclasyoussef@gmail.com>
2019-02-16 17:41:48 +0200

    integration firebase phase 0

--- Data Source/src/main/java/com/example/localdatasourcemodule/ServiceLocator/FirebaseFunctionServiceLocator.java
+++ Data Source/src/main/java/com/example/localdatasourcemodule/ServiceLocator/FirebaseFunctionServiceLocator.java
@@ -1,8 +1,5 @@
 package com.example.localdatasourcemodule.ServiceLocator;
 
-import com.example.localdatasourcemodule.LocalDatabase.Services.InvoiceHeaderService;
-import com.example.localdatasourcemodule.LocalDatabase.Services.InvoiceItemService;
-
 public class FirebaseFunctionServiceLocator {
 
 
